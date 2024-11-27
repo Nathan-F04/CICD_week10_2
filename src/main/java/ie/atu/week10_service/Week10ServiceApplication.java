440625f6d074a7edf940b25890d051c55e63bc31
G00413711@atu.ie <G00413711@atu.ie>
2024-11-27 15:41:53 +0000

    new project created

---
+++ src/main/java/ie/atu/week10_service/Week10ServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week10_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week10_service/Week10ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week10_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
